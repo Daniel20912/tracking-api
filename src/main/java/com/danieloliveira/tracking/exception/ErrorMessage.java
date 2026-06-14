9be357e79210b45f619b8347d24ce7a4f5ca401c
Daniel Oliveira <danieloliveiradejesus2003@gmail.com>
2026-06-14 18:09:36 -0300

    feat: add ErrorMessage

---
+++ src/main/java/com/danieloliveira/tracking/exception/ErrorMessage.java
@@ -0,0 +1,50 @@
+package com.danieloliveira.tracking.exception;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+@ToString
+public class ErrorMessage {
+
+    private final String path;
+    private final String method;
+    private final int status;
+    private final String statusText;
+    private final String message;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Map<String, String> errors;
+
+    public ErrorMessage(HttpServletRequest request, HttpStatus httpStatus, String message) {
+        this.path = request.getRequestURI();
+        this.method = request.getMethod();
+        this.status = httpStatus.value();
+        this.statusText = httpStatus.getReasonPhrase();
+        this.message = message;
+    }
+
+    public ErrorMessage(HttpServletRequest request, HttpStatus httpStatus, String message, BindingResult bindingResult) {
+        this.path = request.getRequestURI();
+        this.method = request.getMethod();
+        this.status = httpStatus.value();
+        this.statusText = httpStatus.getReasonPhrase();
+        this.message = message;
+        addErrors(bindingResult);
+    }
+
+    private void addErrors(BindingResult bindingResult) {
+        this.errors = new HashMap<>();
+        for (FieldError error : bindingResult.getFieldErrors()) {
+            this.errors.put(error.getField(), error.getDefaultMessage());
+        }
+    }
+}
