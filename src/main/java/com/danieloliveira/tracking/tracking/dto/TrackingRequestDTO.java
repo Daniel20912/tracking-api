c1a2382ec996a049629ce132f5fbf580c7f85da1
Daniel Oliveira <danieloliveiradejesus2003@gmail.com>
2026-06-14 18:07:26 -0300

    feat: add validations

--- src/main/java/com/danieloliveira/tracking/tracking/dto/TrackingRequestDTO.java
+++ src/main/java/com/danieloliveira/tracking/tracking/dto/TrackingRequestDTO.java
@@ -1,14 +1,17 @@
 package com.danieloliveira.tracking.tracking.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class TrackingRequestDTO {
+
+    @NotBlank(message = "Code cannot be empty")
     private String code;
 
-    @Email
+    @Email(message = "Invalid email format", regexp = "^[a-zA-Z0-9.+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
     private String email;
 }
