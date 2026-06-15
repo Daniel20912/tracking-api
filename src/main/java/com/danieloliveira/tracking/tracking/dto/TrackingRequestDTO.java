065380ca9d028737d7ac646a24d62be501d53b2f
Daniel Oliveira <danieloliveiradejesus2003@gmail.com>
2026-06-15 18:42:57 -0300

    fix: add code validation

--- src/main/java/com/danieloliveira/tracking/tracking/dto/TrackingRequestDTO.java
+++ src/main/java/com/danieloliveira/tracking/tracking/dto/TrackingRequestDTO.java
@@ -2,6 +2,7 @@ package com.danieloliveira.tracking.tracking.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,6 +10,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public class TrackingRequestDTO {
 
+    @Pattern(regexp = "^[A-Z]{2}[0-9]{9}[A-Z]{2}$", message = "Invalid tracking code format")
     @NotBlank(message = "Code cannot be empty")
     private String code;
 
